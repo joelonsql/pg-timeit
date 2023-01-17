1630403e1ad44057548bb98b6daf15c61ee4adb2
Joel Jakobsson <joel@compiler.org>
2023-01-17 07:54:45 -0300

    Loop forever since it's a daemon
    Before, it returned when there was no more work.
    
    Also add optional input param `return_when_idle`, useful for the unit test,
    since then we want to return.

--- PROCEDURES/work.sql
+++ PROCEDURES/work.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE timeit.work()
+CREATE OR REPLACE PROCEDURE timeit.work(return_when_idle boolean DEFAULT false)
 LANGUAGE plpgsql
 AS $$
 <<fn>>
@@ -23,7 +23,30 @@ declare
     final_result numeric;
     last_run timestamptz;
     overhead_expression text;
+    idle boolean := true;
 begin
+
+loop
+
+    if exists (
+        SELECT 1 FROM timeit.tests
+        WHERE tests.test_state <> 'final'
+    ) then
+        if idle then
+            raise notice 'working';
+            idle := false;
+        end if;
+    else
+        if not idle then
+            raise notice 'idle';
+            idle := true;
+        end if;
+        if return_when_idle then
+            return;
+        end if;
+        perform pg_sleep(1);
+        continue;
+    end if;
 
     --
     -- Use '1' as `test_expression` for the overhead measurement,
@@ -37,135 +60,138 @@ begin
     --
     overhead_expression := '1';
 
-for
-    id,
-    test_state,
-    test_expression,
-    input_types,
-    input_values,
-    significant_figures,
-    base_overhead_time,
-    base_test_time,
-    executions,
-    test_time_1,
-    overhead_time_1,
-    test_time_2,
-    overhead_time_2
-in
-    SELECT
-        tests.id,
-        tests.test_state,
-        tests.test_expression,
-        tests.input_types,
-        tests.input_values,
-        tests.significant_figures,
-        tests.base_overhead_time,
-        tests.base_test_time,
-        tests.executions,
-        tests.test_time_1,
-        tests.overhead_time_1,
-        tests.test_time_2,
-        tests.overhead_time_2
-    FROM timeit.tests
-    WHERE tests.test_state <> 'final'
-    ORDER BY tests.last_run NULLS FIRST
-loop
+    for
+        id,
+        test_state,
+        test_expression,
+        input_types,
+        input_values,
+        significant_figures,
+        base_overhead_time,
+        base_test_time,
+        executions,
+        test_time_1,
+        overhead_time_1,
+        test_time_2,
+        overhead_time_2
+    in
+        SELECT
+            tests.id,
+            tests.test_state,
+            tests.test_expression,
+            tests.input_types,
+            tests.input_values,
+            tests.significant_figures,
+            tests.base_overhead_time,
+            tests.base_test_time,
+            tests.executions,
+            tests.test_time_1,
+            tests.overhead_time_1,
+            tests.test_time_2,
+            tests.overhead_time_2
+        FROM timeit.tests
+        WHERE tests.test_state <> 'final'
+        ORDER BY tests.last_run NULLS FIRST
+    loop
 
-    begin
+        begin
 
-        if test_state = 'init' then
+            if test_state = 'init' then
 
-            RAISE NOTICE 'test id % initialized', id;
+                raise notice 'test id % initialized', id;
 
-            base_overhead_time := timeit.measure(overhead_expression, input_types, input_values, 1);
-            base_test_time := timeit.measure(test_expression, input_types, input_values, 1);
-            executions := greatest(
-                1,
-                (10 ^ significant_figures) * (base_overhead_time / base_test_time)
-            );
-
-            UPDATE timeit.tests SET
-                test_state = 'run_test_1',
-                base_overhead_time = fn.base_overhead_time,
-                base_test_time = fn.base_test_time,
-                executions = fn.executions,
-                last_run = clock_timestamp()
-            WHERE tests.id = fn.id;
-
-        elsif test_state = 'run_test_1' then
-
-            test_time_1 := timeit.measure(test_expression, input_types, input_values, executions);
-            overhead_time_1 := timeit.measure(overhead_expression, input_types, input_values, executions);
-
-            UPDATE timeit.tests SET
-                test_state = 'run_test_2',
-                test_time_1 = fn.test_time_1,
-                overhead_time_1 = fn.overhead_time_1,
-                last_run = clock_timestamp()
-            WHERE tests.id = fn.id;
-
-        elsif test_state = 'run_test_2' then
-
-            test_time_2 := timeit.measure(test_expression, input_types, input_values, executions);
-            overhead_time_2 := timeit.measure(overhead_expression, input_types, input_values, executions);
-
-            net_time_1 := test_time_1 - overhead_time_1;
-            net_time_2 := test_time_2 - overhead_time_2;
-
-            if
-                least(net_time_1,net_time_2)
-                >
-                base_overhead_time * (10 ^ significant_figures)
-            and
-                timeit.round_to_sig_figs(net_time_1, significant_figures)
-                =
-                timeit.round_to_sig_figs(net_time_2, significant_figures)
-            then
-                final_result := timeit.round_to_sig_figs(
-                    (net_time_1 + net_time_2) / (2 * executions)::numeric,
-                    significant_figures
+                base_overhead_time := timeit.measure(overhead_expression, input_types, input_values, 1);
+                base_test_time := timeit.measure(test_expression, input_types, input_values, 1);
+                executions := greatest(
+                    1,
+                    (10 ^ significant_figures) * (base_overhead_time / base_test_time)
                 );
 
                 UPDATE timeit.tests SET
-                    test_state = 'final',
-                    test_time_2 = fn.test_time_2,
-                    overhead_time_2 = fn.overhead_time_2,
-                    final_result = fn.final_result,
+                    test_state = 'run_test_1',
+                    base_overhead_time = fn.base_overhead_time,
+                    base_test_time = fn.base_test_time,
+                    executions = fn.executions,
                     last_run = clock_timestamp()
                 WHERE tests.id = fn.id;
 
-                RAISE NOTICE 'test id % finalized', id;
+            elsif test_state = 'run_test_1' then
+
+                test_time_1 := timeit.measure(test_expression, input_types, input_values, executions);
+                overhead_time_1 := timeit.measure(overhead_expression, input_types, input_values, executions);
+
+                UPDATE timeit.tests SET
+                    test_state = 'run_test_2',
+                    test_time_1 = fn.test_time_1,
+                    overhead_time_1 = fn.overhead_time_1,
+                    last_run = clock_timestamp()
+                WHERE tests.id = fn.id;
+
+            elsif test_state = 'run_test_2' then
+
+                test_time_2 := timeit.measure(test_expression, input_types, input_values, executions);
+                overhead_time_2 := timeit.measure(overhead_expression, input_types, input_values, executions);
+
+                net_time_1 := test_time_1 - overhead_time_1;
+                net_time_2 := test_time_2 - overhead_time_2;
+
+                if
+                    least(net_time_1,net_time_2)
+                    >
+                    base_overhead_time * (10 ^ significant_figures)
+                and
+                    timeit.round_to_sig_figs(net_time_1, significant_figures)
+                    =
+                    timeit.round_to_sig_figs(net_time_2, significant_figures)
+                then
+
+                    final_result := timeit.round_to_sig_figs(
+                        (net_time_1 + net_time_2) / (2 * executions)::numeric,
+                        significant_figures
+                    );
+
+                    UPDATE timeit.tests SET
+                        test_state = 'final',
+                        test_time_2 = fn.test_time_2,
+                        overhead_time_2 = fn.overhead_time_2,
+                        final_result = fn.final_result,
+                        last_run = clock_timestamp()
+                    WHERE tests.id = fn.id;
+
+                    raise notice 'test id % finalized', id;
+
+                else
+
+                    executions := executions * 2;
+
+                    UPDATE timeit.tests SET
+                        test_state = 'run_test_1',
+                        executions = fn.executions,
+                        test_time_2 = fn.test_time_2,
+                        overhead_time_2 = fn.overhead_time_2,
+                        last_run = clock_timestamp()
+                    WHERE tests.id = fn.id;
+
+                end if;
 
             else
 
-                executions := executions * 2;
-
-                UPDATE timeit.tests SET
-                    test_state = 'run_test_1',
-                    executions = fn.executions,
-                    test_time_2 = fn.test_time_2,
-                    overhead_time_2 = fn.overhead_time_2,
-                    last_run = clock_timestamp()
-                WHERE tests.id = fn.id;
+                continue;
 
             end if;
 
-        else
+        exception when others then
 
-            continue;
+            UPDATE timeit.tests SET
+                test_state = 'final',
+                error = SQLERRM
+            WHERE tests.id = fn.id;
 
-        end if;
+        end;
 
-    exception when others then
+        COMMIT;
 
-        UPDATE timeit.tests SET
-            test_state = 'final',
-            error = SQLERRM
-        WHERE tests.id = fn.id;
-
-    end;
-
-    COMMIT;
+    end loop;
 
 end loop;
 
--- sql/async.sql
+++ sql/async.sql
@@ -1,34 +1,18 @@
 CREATE EXTENSION timeit;
 
+--
+-- Enqueue new test.
+--
 SELECT timeit.async('pg_sleep(0.1)');
 
-SELECT
-    id,
-    test_state,
-    executions,
-    final_result
-FROM timeit.tests;
+--
+-- Work until there is no more work.
+--
+CALL timeit.work(return_when_idle := true);
 
-CALL timeit.work();
-
-SELECT
-    id,
-    test_state,
-    executions,
-    final_result
-FROM timeit.tests;
-
-CALL timeit.work();
-
-SELECT
-    id,
-    test_state,
-    executions,
-    final_result
-FROM timeit.tests;
-
-CALL timeit.work();
-
+--
+-- Have a look at test results.
+--
 SELECT
     id,
     test_state,
