22fb64e6b92e3fb55ecc4dde73668d971e3957ed
Joel Jakobsson <joel@compiler.org>
2023-11-09 14:59:09 +0100

    Start at significant_figures=1, then go up if necessary to get a difference

--- FUNCTIONS/cmp.sql
+++ FUNCTIONS/cmp.sql
@@ -11,10 +11,8 @@ CREATE OR REPLACE FUNCTION timeit.cmp(
     function_name_b text,
     input_values_a text[] DEFAULT ARRAY[]::text[],
     input_values_b text[] DEFAULT ARRAY[]::text[],
-    significant_figures integer DEFAULT 1,
-    timeout interval DEFAULT NULL,
-    attempts integer DEFAULT 1,
-    min_time interval DEFAULT '10 ms'::interval
+    min_time interval DEFAULT '10 ms',
+    timeout interval DEFAULT '1 second'
 )
 RETURNS RECORD
 LANGUAGE plpgsql
@@ -29,6 +27,7 @@ declare
     overhead_time_a_2 bigint;
     overhead_time_b_1 bigint;
     overhead_time_b_2 bigint;
+    avg_overhead_time bigint;
     total_time_a_1 bigint;
     total_time_a_2 bigint;
     total_time_b_1 bigint;
@@ -37,21 +36,16 @@ declare
     execution_time_a_2 numeric;
     execution_time_b_1 numeric;
     execution_time_b_2 numeric;
-    overhead_expression text;
-    remaining_attempts integer;
-    min_t bigint := extract(epoch from min_time) * 1e6;
+    t_a_1 numeric;
+    t_a_2 numeric;
+    t_b_1 numeric;
+    t_b_2 numeric;
+    significant_figures int;
 begin
 
-    if num_nulls(function_name_a,function_name_b,input_values_a,input_values_b,significant_figures) <> 0
+    if num_nulls(function_name_a,function_name_b,input_values_a,input_values_b) <> 0
     then
         raise exception 'no arguments must be null';
-    end if;
-    if significant_figures < 1 then
-        raise exception 'significant_figures must be positive';
-    end if;
-
-    if not timeout > min_time * 2 then
-        raise exception 'timeout must be larger than at least twice the min_time';
     end if;
 
     executions := GREATEST(
@@ -59,7 +53,7 @@ begin
         timeit.min_executions(function_name_b, input_values_b, min_time)
     );
 
-    remaining_attempts := attempts;
+    significant_figures := 1;
 
     loop
 
@@ -67,24 +61,22 @@ begin
         overhead_time_a_1 := timeit.overhead(executions);
         test_time_b_1 := timeit.measure(function_name_b, input_values_b, executions);
         overhead_time_b_1 := timeit.overhead(executions);
-
         test_time_a_2 := timeit.measure(function_name_a, input_values_a, executions);
         overhead_time_a_2 := timeit.overhead(executions);
         test_time_b_2 := timeit.measure(function_name_b, input_values_b, executions);
         overhead_time_b_2 := timeit.overhead(executions);
 
-        total_time_a_1 := test_time_a_1 - overhead_time_a_1;
-        total_time_b_1 := test_time_b_1 - overhead_time_b_1;
-        total_time_a_2 := test_time_a_2 - overhead_time_a_2;
-        total_time_b_2 := test_time_b_2 - overhead_time_b_2;
+        avg_overhead_time := (overhead_time_a_1 + overhead_time_b_1 + overhead_time_a_2 + overhead_time_b_2) / 4;
 
-        total_time_a := (total_time_a_1 + total_time_a_2) / 2;
-        total_time_b := (total_time_b_1 + total_time_b_2) / 2;
+        total_time_a_1 := test_time_a_1 - avg_overhead_time;
+        total_time_b_1 := test_time_b_1 - avg_overhead_time;
+        total_time_a_2 := test_time_a_2 - avg_overhead_time;
+        total_time_b_2 := test_time_b_2 - avg_overhead_time;
 
-        execution_time_a_1 := timeit.round_to_sig_figs(total_time_a_1::numeric / (executions * 1e6)::numeric, significant_figures);
-        execution_time_b_1 := timeit.round_to_sig_figs(total_time_b_1::numeric / (executions * 1e6)::numeric, significant_figures);
-        execution_time_a_2 := timeit.round_to_sig_figs(total_time_a_2::numeric / (executions * 1e6)::numeric, significant_figures);
-        execution_time_b_2 := timeit.round_to_sig_figs(total_time_b_2::numeric / (executions * 1e6)::numeric, significant_figures);
+        t_a_1 := timeit.round_to_sig_figs(total_time_a_1, significant_figures);
+        t_a_2 := timeit.round_to_sig_figs(total_time_a_2, significant_figures);
+        t_b_1 := timeit.round_to_sig_figs(total_time_b_1, significant_figures);
+        t_b_2 := timeit.round_to_sig_figs(total_time_b_2, significant_figures);
 
         execution_time_a := timeit.round_to_sig_figs(
             (total_time_a_1 + total_time_a_2)::numeric / (2 * executions * 1e6)::numeric,
@@ -95,50 +87,43 @@ begin
             significant_figures
         );
 
+        total_time_a := (total_time_a_1 + total_time_a_2) / 2;
+        total_time_b := (total_time_b_1 + total_time_b_2) / 2;
+
         raise notice '% vs % (% executions) [%,%] vs [%,%]',
             timeit.pretty_time(execution_time_a),
             timeit.pretty_time(execution_time_b),
             executions,
-            timeit.pretty_time(execution_time_a_1),
-            timeit.pretty_time(execution_time_a_2),
-            timeit.pretty_time(execution_time_b_1),
-            timeit.pretty_time(execution_time_b_2);
+            t_a_1,
+            t_a_2,
+            t_b_1,
+            t_b_2;
 
-
-        if  execution_time_a_1 = execution_time_a_2
-        and execution_time_b_1 = execution_time_b_2
-        then
-            return;
-        end if;
-
+        -- Return anyway if we're near timeout.
         if
             least(total_time_a_1,total_time_a_2,total_time_b_1,total_time_b_2) * 2
             >
             extract(epoch from timeout) * 1e6
         then
-
-            executions := GREATEST(
-                timeit.min_executions(function_name_a, input_values_a, min_time),
-                timeit.min_executions(function_name_b, input_values_b, min_time)
-            );
-            if remaining_attempts = 0 then
-                remaining_attempts := attempts;
-                significant_figures := significant_figures - 1;
-                if significant_figures < 1 then
-                    raise notice 'timeout, returning final result anyway';
-                    return;
-                end if;
-                raise notice 'timeout, will try significant_figures %', significant_figures;
-            else
-                remaining_attempts := remaining_attempts - 1;
-                raise notice 'timeout, % remaining attempts at same precision', remaining_attempts;
-            end if;
-
-            continue;
-
+            return;
         end if;
 
-        executions := executions * 2;
+        -- Double the number of executions until measurements
+        -- are non-zero and converge.
+        if  t_a_1 > 0
+        and t_b_1 > 0
+        and t_a_1 = t_a_2
+        and t_b_1 = t_b_2
+        then
+            -- Increase sig. figs until we can tell a difference.
+            if t_a_1 = t_b_1 then
+                significant_figures := significant_figures + 1;
+            else
+                return;
+            end if;
+        else
+            executions := executions * 2;
+        end if;
 
     end loop;
 
