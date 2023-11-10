4e74d09d59d2052cc7a3c8b727550749419b7ab3
Joel Jakobsson <joel@compiler.org>
2023-11-10 08:42:16 +0100

    Speed-up further

--- FUNCTIONS/cmp.sql
+++ FUNCTIONS/cmp.sql
@@ -9,7 +9,7 @@ CREATE OR REPLACE FUNCTION timeit.cmp(
     function_name_b text,
     input_values_a text[] DEFAULT ARRAY[]::text[],
     input_values_b text[] DEFAULT ARRAY[]::text[],
-    timeout interval DEFAULT '100 ms'::interval
+    timeout interval DEFAULT '1 ms'::interval
 )
 RETURNS RECORD
 LANGUAGE plpgsql
@@ -24,15 +24,6 @@ declare
     test_time_b_2 bigint;
     test_time_b_3 bigint;
     test_time_b_4 bigint;
-    overhead_time_a_1 bigint;
-    overhead_time_a_2 bigint;
-    overhead_time_a_3 bigint;
-    overhead_time_a_4 bigint;
-    overhead_time_b_1 bigint;
-    overhead_time_b_2 bigint;
-    overhead_time_b_3 bigint;
-    overhead_time_b_4 bigint;
-    avg_overhead_time bigint;
     total_time_a_1 bigint;
     total_time_a_2 bigint;
     total_time_a_3 bigint;
@@ -41,6 +32,7 @@ declare
     total_time_b_2 bigint;
     total_time_b_3 bigint;
     total_time_b_4 bigint;
+    overhead_time bigint;
     t0 timestamptz;
 begin
 
@@ -55,50 +47,32 @@ begin
     loop
 
         test_time_a_1 := timeit.measure(function_name_a, input_values_a, executions);
-        overhead_time_a_1 := timeit.overhead(executions);
         test_time_b_1 := timeit.measure(function_name_b, input_values_b, executions);
-        overhead_time_b_1 := timeit.overhead(executions);
         test_time_a_2 := timeit.measure(function_name_a, input_values_a, executions);
-        overhead_time_a_2 := timeit.overhead(executions);
         test_time_b_2 := timeit.measure(function_name_b, input_values_b, executions);
-        overhead_time_b_2 := timeit.overhead(executions);
+        overhead_time := timeit.overhead(executions);
         test_time_a_3 := timeit.measure(function_name_a, input_values_a, executions);
-        overhead_time_a_3 := timeit.overhead(executions);
         test_time_b_3 := timeit.measure(function_name_b, input_values_b, executions);
-        overhead_time_b_3 := timeit.overhead(executions);
         test_time_a_4 := timeit.measure(function_name_a, input_values_a, executions);
-        overhead_time_a_4 := timeit.overhead(executions);
         test_time_b_4 := timeit.measure(function_name_b, input_values_b, executions);
-        overhead_time_b_4 := timeit.overhead(executions);
 
-        avg_overhead_time := (overhead_time_a_1 + overhead_time_b_1 + overhead_time_a_2 + overhead_time_b_2 + overhead_time_a_3 + overhead_time_b_3 + overhead_time_a_4 + overhead_time_b_4) / 8;
-
-        total_time_a_1 := test_time_a_1 - avg_overhead_time;
-        total_time_b_1 := test_time_b_1 - avg_overhead_time;
-        total_time_a_2 := test_time_a_2 - avg_overhead_time;
-        total_time_b_2 := test_time_b_2 - avg_overhead_time;
-        total_time_a_3 := test_time_a_3 - avg_overhead_time;
-        total_time_b_3 := test_time_b_3 - avg_overhead_time;
-        total_time_a_4 := test_time_a_4 - avg_overhead_time;
-        total_time_b_4 := test_time_b_4 - avg_overhead_time;
+        total_time_a_1 := test_time_a_1 - overhead_time;
+        total_time_b_1 := test_time_b_1 - overhead_time;
+        total_time_a_2 := test_time_a_2 - overhead_time;
+        total_time_b_2 := test_time_b_2 - overhead_time;
+        total_time_a_3 := test_time_a_3 - overhead_time;
+        total_time_b_3 := test_time_b_3 - overhead_time;
+        total_time_a_4 := test_time_a_4 - overhead_time;
+        total_time_b_4 := test_time_b_4 - overhead_time;
 
         total_time_a := (total_time_a_1 + total_time_a_2 + total_time_a_3 + total_time_a_4);
         total_time_b := (total_time_b_1 + total_time_b_2 + total_time_b_3 + total_time_b_4);
 
-        raise notice '% executions [%,%,%,%] vs [%,%,%,%]',
-            executions,
-            total_time_a_1,
-            total_time_a_2,
-            total_time_a_3,
-            total_time_a_4,
-            total_time_b_1,
-            total_time_b_2,
-            total_time_b_3,
-            total_time_b_4;
-
-        if greatest(total_time_a_1,total_time_a_2,total_time_a_3,total_time_a_4) < least(total_time_b_1,total_time_b_2,total_time_b_3,total_time_b_4)
-        or least(total_time_a_1,total_time_a_2,total_time_a_3,total_time_a_4) > greatest(total_time_b_1,total_time_b_2,total_time_b_3,total_time_b_4)
-        or clock_timestamp()-t0 > timeout
+        if least(total_time_a_1,total_time_a_2,total_time_a_3,total_time_a_4) > 0
+        and least(total_time_b_1,total_time_b_2,total_time_b_3,total_time_b_4) > 0
+        and (greatest(total_time_a_1,total_time_a_2,total_time_a_3,total_time_a_4) < least(total_time_b_1,total_time_b_2,total_time_b_3,total_time_b_4)
+            or least(total_time_a_1,total_time_a_2,total_time_a_3,total_time_a_4) > greatest(total_time_b_1,total_time_b_2,total_time_b_3,total_time_b_4)
+            or clock_timestamp()-t0 > timeout)
         then
             executions := executions * 4;
             return;
